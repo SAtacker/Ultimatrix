83f5a4235de5c13e0400881df6f73c86d99b2f2e
Shreyas Atre <shreyasatre16@gmail.com>
2020-05-26 14:16:54 +0530

    bugs

--- update/blog.py
+++ update/blog.py
@@ -2,15 +2,26 @@ import os
 from flask import Flask, flash, request, redirect, url_for,render_template
 from werkzeug.utils import secure_filename
 from flask.helpers import send_from_directory
-
+from flask_sqlalchemy import SQLAlchemy
 
 UPLOAD_FOLDER = r"C:\Users\shrey_imghzs\Desktop\Ultimatrix\update\static\files"
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','zip'}
+
+
 
 
 app = Flask(__name__,template_folder="")
-
+db = SQLAlchemy(app) 
 app.config['SECRET_KEY'] = 'cefvnnvjnncewbfewbvbew'
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///files.db' 
+
+class Add_Filename(db.Model): 
+    id = db.Column(db.Integer, primary_key=True) 
+    text = db.Column(db.String(200)) 
+  
+    def __repr__(self): 
+        return self.text 
 
 @app.route('/')
 def index():
@@ -23,8 +34,11 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route('/blog.html', methods=['GET', 'POST'])
 def upload_file():
+    f=None
+    filename = ''
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -39,14 +53,20 @@ def upload_file():
         if allowed_file(file.filename):
             if file:
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                fname=Add_Filename(text=filename)
+                db.session.add(fname)
+                db.session.commit()
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(file_path)
                 flash('Your file is uploaded successfully',category='message')
         else:
             flash('Selected file is not permitted')
-    return render_template('blog.html')
+    return render_template('blog.html',filename=filename)
 
 from flask import send_from_directory
-
+@app.route('/<filename>',methods = ['GET','POST'])
+def download(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename=filename,as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
